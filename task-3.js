36e4882767d9f1ef2dc11a75c59b9c863e4a97b1
rimonamdadul-collab <rimonamdadul@gmail.com>
2026-02-07 22:17:59 +0600

    add the task-3

---
+++ task-3.js
@@ -0,0 +1,10 @@
+function arry (numbers){
+    
+   const diff =  numbers.length;
+   console.log(diff)
+return diff
+  
+}
+const number = [29,99,29,93]
+const nums = arry(number)
+console.log(nums)
