66acad2fcb190cd3ebf72b05405dfddc05547a9a
rimonamdadul-collab <rimonamdadul@gmail.com>
2026-02-07 18:27:41 +0600

    added the arry-operation

---
+++ arry-operation.js
@@ -0,0 +1,21 @@
+function sumOfNumbers (numbers){
+    let sum =0;
+    for (const number of numbers ){
+        sum = sum + number;
+    }
+return sum;
+}
+const nums = [22,42,53,763]
+const sum = sumOfNumbers(nums)
+console.log(sum)
+function sumOfNumbers1 (numbers){
+    let sum  =0;
+    for (number of numbers) {
+        sum = sum + number;
+
+    }
+        return sum;
+    
+}
+const sum1 = sumOfNumbers1([12,454,654,25])
+console.log(sum1)
