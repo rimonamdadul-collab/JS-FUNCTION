424bedabfcba7f0b2732bf41686d4445b6c64921
rimonamdadul-collab <rimonamdadul@gmail.com>
2026-02-07 13:45:39 +0600

    aded the argument

---
+++ argument.js
@@ -0,0 +1,18 @@
+function doublet (number){
+    const double = number * 2;
+    console.log(number, double)
+}
+doublet (15)
+console.log('--------------')
+doublet (35)
+console.log('=====================')
+const mony = 55;
+doublet(number);
+
+function difference (num1, num2){
+    const diff = num1 - num2;
+    console.log(num1,num2, diff)
+}
+const fatherage = 40;
+const myage = 10;
+difference(fatherage, myage)
