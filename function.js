59330a12a52c1a74de2e67224475240b2b0cc4b8
rimonamdadul-collab <rimonamdadul@gmail.com>
2026-02-07 11:01:25 +0600

    function file the add

---
+++ function.js
@@ -0,0 +1,14 @@
+const mony = 45;
+
+function Fanoffkor() {
+ console.log('bosa theke uthe dara')
+console.log('walk towards the switch')
+console.log('click the switch to off of the fan')
+}
+Fanoffkor();
+function brushteeth(){
+    console.log('pick up the brush')
+     console.log('add paste')
+      console.log('2min goso and goso and pena felo')
+}
+brushteeth();
