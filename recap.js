f84f2197d77308c4b9335c77af90790e2e9ff9c6
rimonamdadul-collab <rimonamdadul@gmail.com>
2026-02-07 16:02:03 +0600

    add an the recap

---
+++ recap.js
@@ -0,0 +1,43 @@
+function tentimes (number){
+    const reacap = number *5;
+    return reacap;
+
+}
+const output  = tentimes(5)
+// console.log(output)
+function add (price1, price2){
+    const add1 = price1 + price2;
+    return add1;
+}
+const bach = 10;
+const vaat = 5;
+const output1 = bach + vaat;
+console.log(output1)
+function domath (num1, num2){
+    const sum = num1 + num2;
+    const diff = num1 - num2
+    const multiply = sum * diff;
+    const result = multiply/2;
+return result;
+}
+const dill = domath(10, 20)
+console.log(dill)
+function isEven(number){
+    if (number % 2 ===0){
+        return true;
+
+    }
+    else{
+        return false;
+    }
+}
+console.log(isEven(2))
+console.log(isEven(7))
+function isEven1(number){
+    if (number % 2 === 1){
+        return true;
+    }
+    return false;
+}
+console.log(isEven1(58))
+console.log(isEven1(9))
