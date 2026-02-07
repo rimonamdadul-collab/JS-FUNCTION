df41a2b5e4759a42f2e4b8cff2fbee86e0f4d88e
rimonamdadul-collab <rimonamdadul@gmail.com>
2026-02-07 16:22:57 +0600

    add an the conditional

---
+++ conditional.js
@@ -0,0 +1,18 @@
+function isEven (number){
+    if (number % 2===0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+console.log(isEven(4))
+console.log(isEven(9))
+function isOdd(number){
+    if (number % 2 === 1){
+        return true
+    }
+    return false
+}
+console.log(isOdd(7))
+console.log(isOdd(3))
