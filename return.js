7c05cfe9eeddc65bce8200f4a7a7ae20a534f823
rimonamdadul-collab <rimonamdadul@gmail.com>
2026-02-07 14:42:47 +0600

    added the return

---
+++ return.js
@@ -0,0 +1,14 @@
+function tentimes(number){
+    const ten = number * 10;
+    return ten;
+}
+const output = tentimes(5);
+console.log(output);
+function chata (number){
+const half = number / 2;
+return half ;
+}
+const big = chata(10)
+console.log(big)
+const bignumber = tentimes(99);
+console.log(bignumber)
