3f6997a08f0a18b3724000b45bd4089a5fa82b53
rimonamdadul-collab <rimonamdadul@gmail.com>
2026-02-07 11:35:01 +0600

    adde the parameters

---
+++ parameter.js
@@ -0,0 +1,16 @@
+
+
+function square(number){
+    console.log(number)
+const borgo = number * number;
+console.log('given numbers is', borgo)
+}
+square(4);
+square(49);
+square(324),
+square(30)
+function add (num1, num2){
+    const numbers = num1 + num2;
+    console.log(numbers)
+}
+add (23,15)
